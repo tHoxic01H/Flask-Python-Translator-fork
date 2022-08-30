124ffad3e25525ab61a1f0b3fa32b55623d002b7
Cedric <cedrictianarabarijohn@gmail.com>
2022-08-30 16:13:37 +0400

    Test

--- module/services/translation/translation_service.py
+++ module/services/translation/translation_service.py
@@ -1,4 +1,5 @@
-import translators as ts
+# import translators as ts
 
 def translate_from_google(phrase, from_language, to_language):
-    return ts.google(phrase, from_language, to_language)
+    # return ts.google(phrase, from_language, to_language)
+    return "Hello you"
