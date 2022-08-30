41d6d6833db03324291ae3c454b8fb02415476b8
Cedric <cedrictianarabarijohn@gmail.com>
2022-08-30 16:45:00 +0400

    using googletrans

--- module/services/translation/translation_service.py
+++ module/services/translation/translation_service.py
@@ -1,5 +1,8 @@
 # import translators as ts
+from googletrans import Translator
 
 def translate_from_google(phrase, from_language, to_language):
     # return ts.google(phrase, from_language, to_language)
-    return "Hello you"
+    translator = Translator()
+    translation = translator.translate(phrase, dest=to_language)
+    return translation.text
